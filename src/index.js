81cc8475df338ee98e4603c19a9b1f44ff4a12bb
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-09 13:56:40 +0600

    Add font awesome.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import './style.scss';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
 require('./assets/logo.png');
 // Create heading node
 const heading = document.createElement('h1');
-heading.textContent = 'Hi there';
+const icon = document.createElement('i');
+icon.classList.add('fa-solid', 'fa-house');
+heading.textContent = 'Hi there ';
 
 // Append heading node to the DOM
 const app = document.querySelector('#root');
 app.append(heading);
+app.append(icon);
