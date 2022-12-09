18cab2962da7219a31cc134cbdfe2e7480659a9b
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-10 03:56:46 +0600

    Update formAddItem function to generate list in html.

--- src/index.js
+++ src/index.js
@@ -3,41 +3,30 @@ import '@fortawesome/fontawesome-free/js/fontawesome.js';
 import '@fortawesome/fontawesome-free/js/solid.js';
 import '@fortawesome/fontawesome-free/js/regular.js';
 import '@fortawesome/fontawesome-free/js/brands.js';
+import TodoList from './modules/TodoList.js';
 
 require('./assets/logo.png');
-// Create heading node
+
 const list = document.querySelector('.list-ul');
-const taskList = [
-  { index: 0, description: 'Fist task', completed: false },
-  { index: 1, description: 'Fist task', completed: false },
-  { index: 2, description: 'Fist task', completed: false },
-  { index: 3, description: 'Fist task', completed: false },
-  { index: 4, description: 'Fist task', completed: false },
-  { index: 5, description: 'Fist task', completed: false },
-];
 
 const sortItems = (items) => {
   const sortedItems = items.sort((a, b) => a.index - b.index);
   return sortedItems;
 };
 
-const generateTaskListHTML = () => {
-  const sortedList = sortItems(taskList);
-  sortedList.forEach((item) => {
-    const todoItem = document.createElement('li');
-    todoItem.classList.add('list-item', 'list-style');
-    todoItem.innerHTML = `
-            <div class="list-item-content">
-              <input  type="checkbox" />
-              <label>${item.description}</label>
-            </div>
-            <div class="list-item-action">
-              <i class="fa-solid fa-trash icon hidden"></i>
-              <i class="fa-solid fa-ellipsis-vertical icon"></i>
-            </div>`;
-    list.appendChild(todoItem);
+// Append  node to the DOM
+formAddItem.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  newTodoList.addTodoItem({
+    description: data.get('description'),
   });
-};
 
-// Append heading node to the DOM
-document.addEventListener('DOMContentLoaded', generateTaskListHTML);
+  formAddItem.reset();
+  generateTaskListHTML(newTodoList.getTodoList());
+});
+
+document.addEventListener(
+  'DOMContentLoaded',
+  generateTaskListHTML(newTodoList.getTodoList())
+);
