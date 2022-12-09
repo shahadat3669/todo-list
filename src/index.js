7da9ba2532bc217fc0477f664a7e086ba07d0e87
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-10 03:59:59 +0600

    Update bindTaskEvents to add event for edit, delete functionality.

--- src/index.js
+++ src/index.js
@@ -16,7 +16,49 @@ const sortItems = (items) => {
   return sortedItems;
 };
 
-const bindTaskEvents = (taskListItem) => {};
+const bindTaskEvents = async (taskListItem) => {
+  const todoItemId = taskListItem.id;
+  const checkBox = taskListItem.querySelector('input[type="checkbox"]');
+  const icon1 = taskListItem.querySelector('.trash');
+  const icon2 = taskListItem.querySelector('.ellipsis');
+  const description = taskListItem.querySelector('input[name="description"]');
+
+  description.addEventListener('click', (event) => {
+    event.target.removeAttribute('readOnly');
+    event.target.classList.add('active-item');
+    icon1.classList.remove('hidden');
+    icon2.classList.add('hidden');
+  });
+  icon1.addEventListener('click', async () => {
+    await newTodoList.removeTodoItem(todoItemId);
+    // eslint-disable-next-line no-use-before-define
+    generateListHTML(newTodoList.getTodoList());
+  });
+
+  description.addEventListener('focusout', (event) => {
+    newTodoList.editToDoItem(todoItemId, { description: event.target.value });
+    description.setAttribute('readonly', true);
+    description.classList.remove('active-item');
+  });
+
+  document.addEventListener('click', (event) => {
+    const isClickInsideElement = taskListItem.contains(event.target);
+    if (!isClickInsideElement) {
+      icon1.classList.add('hidden');
+      icon2.classList.remove('hidden');
+    }
+  });
+
+  checkBox.addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      newTodoList.editToDoItem(todoItemId, { completed: true });
+      checkBox.nextElementSibling.classList.add('strike-through');
+    } else {
+      newTodoList.editToDoItem(todoItemId, { completed: false });
+      checkBox.nextElementSibling.classList.remove('strike-through');
+    }
+  });
+};
 
 const generateListHTML = (taskList) => {
   list.innerHTML = '';
