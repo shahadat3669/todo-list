56eb2c33f3637b70f80b42da66a31b030fc5c968
Shahadat Hossain <shahadat3669@gmail.com>
2022-12-10 03:52:09 +0600

    Create addTodoItem to create a new item and add it in the list.

--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -1,7 +1,17 @@
+import TodoItem from './TodoItem.js';
+
 export default class TodoList {
   constructor() {
     this.todoList = localStorage.getItem('todoList')
       ? JSON.parse(localStorage.getItem('todoList'))
       : [];
+  }
+
+  addTodoItem(todoItem) {
+    todoItem.index = this.todoList.length + 1;
+    todoItem.completed = false;
+    const newTodoItem = new TodoItem(todoItem);
+    this.todoList.push(newTodoItem);
+    localStorage.setItem('todoList', JSON.stringify(this.todoList));
   }
 }
